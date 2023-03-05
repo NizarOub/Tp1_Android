04f3d9a6846526a688eb7f1c022b936b3a3adc54
Nizar Oubbali <nizar.oubbali@gmail.com>
2023-03-05 23:38:04 +0100

    First

---
+++ app/src/main/java/com/example/tp1_android/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.tp1_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView txt = findViewById(R.id.txt1);
+        txt.setText("Here we go");
+    }
+}
