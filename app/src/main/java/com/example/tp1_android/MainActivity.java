8de8a0f57bc21367c0f56721ceaaf509bcc87cdd
Nizar Oubbali <nizar.oubbali@gmail.com>
2023-03-08 21:53:11 +0100

    First

--- app/src/main/java/com/example/tp1_android/MainActivity.java
+++ app/src/main/java/com/example/tp1_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tp1_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView txt = findViewById(R.id.txt1);
         txt.setText("Here we go");
+        Log.d("MainActivity", "onCreate() called ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart() called ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity", "onResume() called ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "onPause() called ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop() called ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy() called ");
     }
 }
